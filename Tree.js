9d4352f280170b2e4e3d1b1ed4fc27b16a803dcd
Andrey Karamnoff <karamnoff@gmail.com>
2019-01-21 14:25:25 +0300

    add solution for trees

--- Tree.js
+++ Tree.js
@@ -26,46 +26,7 @@ class Tree {
   }
 
   // BEGIN (write your solution here)
-  hasChildren() {
-    return this.children.size !== 0;
-  }
 
-  hasChild(key) {
-    return this.children.has(key);
-  }
-
-  getParent() {
-    return this.parent;
-  }
-
-  removeChild(key) {
-    return this.children.delete(key);
-  }
-
-  getDeepChild(keys) {
-    if (!keys.length) {
-      return null;
-    }
-
-    const [key] = keys;
-    const child = this.getChild(key);
-
-    if (keys.length === 1) {
-      return child;
-    }
-
-    if (!child) {
-      return null;
-    }
-    return child.getDeepChild(keys.slice(1));
-  }
-
-  getChildren() {
-    const children = [];
-    const values = this.children.values();
-    Array.from(values).forEach(value => children.push(value));
-    return children;
-  }
   // END
 }
 
