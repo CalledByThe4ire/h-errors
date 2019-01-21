0afa39c043915ec2aca4549c666fa8258961f708
Andrey Karamnoff <karamnoff@gmail.com>
2019-01-21 18:53:36 +0300

    add solution for hexlet_fs

--- HexletFs.js
+++ HexletFs.js
@@ -3,6 +3,7 @@ import Tree from 'hexlet-trees';
 
 // BEGIN (write your solution here)
 
+const getPathParts = filePath => filePath.replace(/\/+\B/g, '').trim().split(path.sep).filter(v => v);
 // END
 
 export default class {
@@ -11,7 +12,37 @@ export default class {
   }
 
   // BEGIN (write your solution here)
+  isDirectory(key) {
+    const node = this.findNode(key);
+    if (node && node.meta.type === 'dir') {
+      return true;
+    }
+    return false;
+  }
 
+  isFile(key) {
+    const node = this.findNode(key);
+    if (node && node.meta.type === 'file') {
+      return true;
+    }
+    return false;
+  }
+
+  mkdirSync(filePath) {
+    const filePathParts = getPathParts(filePath);
+    const [dir, value] = filePathParts;
+    const child = typeof value === 'undefined' ? this.tree : this.tree.getDeepChild(dir);
+    child.addChild(typeof value === 'undefined' ? dir : value, { type: 'dir' });
+    return this.tree;
+  }
+
+  touchSync(filePath) {
+    const filePathParts = getPathParts(filePath);
+    const [dir, value] = filePathParts;
+    const child = typeof value === 'undefined' ? this.tree : this.tree.getDeepChild(dir);
+    child.addChild(typeof value === 'undefined' ? dir : value, { type: 'file' });
+    return this.tree;
+  }
   // END
 
   findNode(filepath) {
